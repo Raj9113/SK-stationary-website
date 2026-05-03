1d8c2f6ac37ab2e9009bb5efd292d330097b22a0
Raj9113 <94070105+Raj9113@users.noreply.github.com>
2026-05-03 21:17:09 +0530

    added some rondom produnt

--- src/app/stationery/page.tsx
+++ src/app/stationery/page.tsx
@@ -13,7 +13,7 @@ const Card = ({
 }) => {
   return (
     // Agar aap yahan color change karenge, toh sabhi cards ka color badal jayega
-    <div className="bg-slate-800 p-6 flex flex-col rounded-lg shadow-md">
+    <div className="bg-slate-800 p-6 flex flex-wrap rounded-lg shadow-md">
       <h3 className="text-xl font-bold mb-2 text-green-400">{title}</h3>
       <p className="text-gray-400 mb-4">{description}</p>
       <button  className="border bg-amber-100 rounded-md text-gray-600 mt-auto hover:underline">
@@ -31,28 +31,304 @@ const Product = () => {
           Complete range of office and school supplies.
         </p>
         <Card
-          title="Stationery"
+          title="Books"
           description="Complete range of office and school supplies."
           linkText="View Products &rarr;"
           href="/stationery"
         />
 
         <Card
-          title="Online Services"
+          title="Pens"
           description="Fast assistance with forms, applications, and digital tasks."
           linkText="Learn More &rarr;"
           href="/janseva-kendr"
         />
 
         <Card
-          title="Commercial Shuttering"
+          title="Natraj Pencils"
           description="Reliable materials and great deals for your construction needs."
           linkText="Get a Quote &rarr;"
           href="/shuttering"
         />
 
         <Card
-          title="DJ Services"
+          title="Cardboard"
+          description="Professional sound and music for events and parties."
+          linkText="Book Now &rarr;"
+          href="/dj-services"
+        />
+        <Card
+          title="Cardboard"
+          description="Professional sound and music for events and parties."
+          linkText="Book Now &rarr;"
+          href="/dj-services"
+        />
+        <Card
+          title="Cardboard"
+          description="Professional sound and music for events and parties."
+          linkText="Book Now &rarr;"
+          href="/dj-services"
+        />
+        <Card
+          title="Cardboard"
+          description="Professional sound and music for events and parties."
+          linkText="Book Now &rarr;"
+          href="/dj-services"
+        />
+        <Card
+          title="Cardboard"
+          description="Professional sound and music for events and parties."
+          linkText="Book Now &rarr;"
+          href="/dj-services"
+        />
+        <Card
+          title="Cardboard"
+          description="Professional sound and music for events and parties."
+          linkText="Book Now &rarr;"
+          href="/dj-services"
+        />
+        <Card
+          title="Cardboard"
+          description="Professional sound and music for events and parties."
+          linkText="Book Now &rarr;"
+          href="/dj-services"
+        />
+        <Card
+          title="Cardboard"
+          description="Professional sound and music for events and parties."
+          linkText="Book Now &rarr;"
+          href="/dj-services"
+        />
+        <Card
+          title="Cardboard"
+          description="Professional sound and music for events and parties."
+          linkText="Book Now &rarr;"
+          href="/dj-services"
+        />
+        <Card
+          title="Cardboard"
+          description="Professional sound and music for events and parties."
+          linkText="Book Now &rarr;"
+          href="/dj-services"
+        />
+        <Card
+          title="Cardboard"
+          description="Professional sound and music for events and parties."
+          linkText="Book Now &rarr;"
+          href="/dj-services"
+        />
+        <Card
+          title="Cardboard"
+          description="Professional sound and music for events and parties."
+          linkText="Book Now &rarr;"
+          href="/dj-services"
+        />
+        <Card
+          title="Cardboard"
+          description="Professional sound and music for events and parties."
+          linkText="Book Now &rarr;"
+          href="/dj-services"
+        />
+        <Card
+          title="Cardboard"
+          description="Professional sound and music for events and parties."
+          linkText="Book Now &rarr;"
+          href="/dj-services"
+        />
+        <Card
+          title="Cardboard"
+          description="Professional sound and music for events and parties."
+          linkText="Book Now &rarr;"
+          href="/dj-services"
+        />
+        <Card
+          title="Cardboard"
+          description="Professional sound and music for events and parties."
+          linkText="Book Now &rarr;"
+          href="/dj-services"
+        />
+        <Card
+          title="Cardboard"
+          description="Professional sound and music for events and parties."
+          linkText="Book Now &rarr;"
+          href="/dj-services"
+        />
+        <Card
+          title="Cardboard"
+          description="Professional sound and music for events and parties."
+          linkText="Book Now &rarr;"
+          href="/dj-services"
+        />
+        <Card
+          title="Cardboard"
+          description="Professional sound and music for events and parties."
+          linkText="Book Now &rarr;"
+          href="/dj-services"
+        />
+        <Card
+          title="Cardboard"
+          description="Professional sound and music for events and parties."
+          linkText="Book Now &rarr;"
+          href="/dj-services"
+        />
+        <Card
+          title="Cardboard"
+          description="Professional sound and music for events and parties."
+          linkText="Book Now &rarr;"
+          href="/dj-services"
+        />
+        <Card
+          title="Cardboard"
+          description="Professional sound and music for events and parties."
+          linkText="Book Now &rarr;"
+          href="/dj-services"
+        />
+        <Card
+          title="Cardboard"
+          description="Professional sound and music for events and parties."
+          linkText="Book Now &rarr;"
+          href="/dj-services"
+        />
+        <Card
+          title="Cardboard"
+          description="Professional sound and music for events and parties."
+          linkText="Book Now &rarr;"
+          href="/dj-services"
+        />
+        <Card
+          title="Cardboard"
+          description="Professional sound and music for events and parties."
+          linkText="Book Now &rarr;"
+          href="/dj-services"
+        />
+        <Card
+          title="Cardboard"
+          description="Professional sound and music for events and parties."
+          linkText="Book Now &rarr;"
+          href="/dj-services"
+        />
+        <Card
+          title="Cardboard"
+          description="Professional sound and music for events and parties."
+          linkText="Book Now &rarr;"
+          href="/dj-services"
+        />
+        <Card
+          title="Cardboard"
+          description="Professional sound and music for events and parties."
+          linkText="Book Now &rarr;"
+          href="/dj-services"
+        />
+        <Card
+          title="Cardboard"
+          description="Professional sound and music for events and parties."
+          linkText="Book Now &rarr;"
+          href="/dj-services"
+        />
+        <Card
+          title="Cardboard"
+          description="Professional sound and music for events and parties."
+          linkText="Book Now &rarr;"
+          href="/dj-services"
+        />
+        <Card
+          title="Cardboard"
+          description="Professional sound and music for events and parties."
+          linkText="Book Now &rarr;"
+          href="/dj-services"
+        />
+        <Card
+          title="Cardboard"
+          description="Professional sound and music for events and parties."
+          linkText="Book Now &rarr;"
+          href="/dj-services"
+        />
+        <Card
+          title="Cardboard"
+          description="Professional sound and music for events and parties."
+          linkText="Book Now &rarr;"
+          href="/dj-services"
+        />
+        <Card
+          title="Cardboard"
+          description="Professional sound and music for events and parties."
+          linkText="Book Now &rarr;"
+          href="/dj-services"
+        />
+        <Card
+          title="Cardboard"
+          description="Professional sound and music for events and parties."
+          linkText="Book Now &rarr;"
+          href="/dj-services"
+        />
+        <Card
+          title="Cardboard"
+          description="Professional sound and music for events and parties."
+          linkText="Book Now &rarr;"
+          href="/dj-services"
+        />
+        <Card
+          title="Cardboard"
+          description="Professional sound and music for events and parties."
+          linkText="Book Now &rarr;"
+          href="/dj-services"
+        />
+        <Card
+          title="Cardboard"
+          description="Professional sound and music for events and parties."
+          linkText="Book Now &rarr;"
+          href="/dj-services"
+        />
+        <Card
+          title="Cardboard"
+          description="Professional sound and music for events and parties."
+          linkText="Book Now &rarr;"
+          href="/dj-services"
+        />
+        <Card
+          title="Cardboard"
+          description="Professional sound and music for events and parties."
+          linkText="Book Now &rarr;"
+          href="/dj-services"
+        />
+        <Card
+          title="Cardboard"
+          description="Professional sound and music for events and parties."
+          linkText="Book Now &rarr;"
+          href="/dj-services"
+        />
+        <Card
+          title="Cardboard"
+          description="Professional sound and music for events and parties."
+          linkText="Book Now &rarr;"
+          href="/dj-services"
+        />
+        <Card
+          title="Cardboard"
+          description="Professional sound and music for events and parties."
+          linkText="Book Now &rarr;"
+          href="/dj-services"
+        />
+        <Card
+          title="Cardboard"
+          description="Professional sound and music for events and parties."
+          linkText="Book Now &rarr;"
+          href="/dj-services"
+        />
+        <Card
+          title="Cardboard"
+          description="Professional sound and music for events and parties."
+          linkText="Book Now &rarr;"
+          href="/dj-services"
+        />
+        <Card
+          title="Cardboard"
+          description="Professional sound and music for events and parties."
+          linkText="Book Now &rarr;"
+          href="/dj-services"
+        />
+        <Card
+          title="Cardboard"
           description="Professional sound and music for events and parties."
           linkText="Book Now &rarr;"
           href="/dj-services"
