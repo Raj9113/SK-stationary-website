02d2a78b75775e13aa17bc85cc80df492ff391a4
Raj9113 <94070105+Raj9113@users.noreply.github.com>
2026-05-04 23:55:13 +0530

    decoreted card

--- src/app/stationery/page.tsx
+++ src/app/stationery/page.tsx
@@ -2,28 +2,36 @@ const Card = ({
   title,
   description,
   linkText,
+  linkText2,
   href,
 }: {
   title: string;
   description: string;
   linkText: string;
+  linkText2: string;
   href: string;
 }) => {
   return (
     // Agar aap yahan color change karenge, toh sabhi cards ka color badal jayega
-    <div className="bg-yellow-500 ">
-      <h3 className="text-xl font-bold mb-2 text-green-400">{title}</h3>
-      <p className="text-gray-400 mb-4">{description}</p>
-      <button className="border bg-amber-100 rounded-md text-gray-600 mt-auto hover:underline">
-        {linkText}
-      </button>
+    <div className="bg-green-800/30 backdrop-blur-md p-4 md:rounded-4xl rounded-3xl shadow-md border border-slate-700/50 md:h-70 flex flex-col justify-between">
+      <h3 className="text-xl font-bold text-gray-900">{title}</h3>
+      <p className="text-gray-400">{description}</p>
+      <div className="flex flex-row gap-2">
+        <button className="bg-[#07553b] rounded-2xl w-45 h-8 text-xs text-[#ced46a] hover:bg-black hover:text-gray-100">
+          {linkText}
+        </button>
+        <button className="bg-green-700 rounded-2xl w-45 h-8 text-xs text-[#ea1591] hover:bg-black hover:text-gray-100">
+          {linkText2}
+        </button>
+      </div>
     </div>
   );
 };
 
 const Product = () => {
   // 1. Ek variable jisse sabhi buttons ka naam ek saath change hoga
-  const BUTTON_TEXT = "Buy Now";
+  const BUTTON1_TEXT = "Add to Cart";
+  const BUTTON2_TEXT = "Buy Now";
 
   // 2. Aapke products ka data (Future mein yeh MongoDB se aayega)
   const productsData = [
@@ -295,21 +303,23 @@ const Product = () => {
     // Bas yahan naye products add karte jaiye, HTML mein kuch change nahi karna padega
   ];
   return (
-    <div className="min-h-screen bg-linear-to-r from-violet-200 to-pink-200 text-gray-800 py-10">
+    <div className="bg-[url('/Img/bg-1.jpg')] bg-cover bg-fixed bg-center bg-no-repeat py-12">
       <div className="container mx-auto px-4">
         <h1 className="text-3xl font-bold mb-6">Stationery</h1>
         <p className="text-lg text-gray-300 mb-8">
           Complete range of office and school supplies.
         </p>
-        <div className="grid md:grid-cols-6 gap-2 grid-cols-2">
-          <div className="grid md:grid-cols-6 gap-2 grid-cols-2">
+        <div className="">
+          <div className="grid md:grid-cols-4 md:gap-16 gap-6 grid-cols-2 p-6 md:px-30">
             {/* Yeh code array mein jitne products honge, utne Card apne aap bana dega */}
             {productsData.map((product) => (
               <Card
                 key={product.id}
                 title={product.title}
                 description={product.description}
-                linkText={BUTTON_TEXT} // Yahan aapka variable use ho raha hai
+                linkText={BUTTON1_TEXT}
+                linkText2={BUTTON2_TEXT}
+                // Yahan aapka variable use ho raha hai
                 href={product.href}
               />
             ))}
